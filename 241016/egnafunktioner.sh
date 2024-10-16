c7bb3697457881568c3f6e50c813e41a5810941b
Kent Larsson <kent@c8.se>
2024-10-16 11:15:37 +0200

    autocommit complete 1115 (37)

--- 241016/egnafunktioner.sh
+++ 241016/egnafunktioner.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Om filen endast innehåller funktioner som jag vill använda,
+# så ska inga funktionsanrop göras när den läses. Det innebär
+# att endast funktionens definition finns i filen.
+
 greet() {
     for namn in "$@"; do # "$@" => "$1" "$2" "$3" ...
         echo "Hej1 $namn"
