757fecffb5b43b58b1c03d0d81c530d0a185860f
Kent Larsson <kent@c8.se>
2024-10-18 09:44:34 +0200

    autocommit complete 0944 (34)

--- 241018/psuedokod.py
+++ 241018/psuedokod.py
@@ -20,5 +20,7 @@
 # Mål: Byt ut repeterande specialtecken som :,.; och mellanslag mot endast ett tecken i
 # en fil som användaren anger och skriv ut det till stdout.
 
+non_repeating_chars = ":,.; "
+
 # för varje line i file_lines
 #
