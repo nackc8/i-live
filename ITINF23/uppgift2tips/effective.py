76110b87a78977dbf1f59edee1d00f13f3fbb991
Kent Larsson <kent@c8.se>
2024-10-22 19:17:12 +0200

    autocommit complete 1917 (12)

--- ITINF23/uppgift2tips/effective.py
+++ ITINF23/uppgift2tips/effective.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+# | Long^1          | Short^2 | Indicator^3     | Används i                  |
+# | --------------- | ------- | --------------- | -------------------------- |
+# | `--hash`        | `-h`    | `#`             | [Bash][1], [Python][2]     |
+# | `--dblslash`    | `-l`    | `//`            | [C][3], [Java][4]          |
+# | `--dbldash`     | `-d`    | `--`            | [SQL][5], [Lua][6]         |
+# | `--percent`     | `-p`    | `%`             | [LaTeX][7], [MATLAB][8]    |
+# | `--apostrophe`  | `-a`    | `'`             | [Visual Basic][9]          |
+# | `--basic`       | `-b`    | `REM` eller `'` | [BASIC][10]                |
+# | `--semicolon`   | `-s`    | `;`             | [Lisp][11], [Assembly][12] |
+# | `--exclamation` | `-e`    | `!`             | [Fortran][13]              |
+
+
 class Format:
     def __init__(self, )
