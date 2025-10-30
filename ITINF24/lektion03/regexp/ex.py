4634797bf0a245e20179bb39f4f3cd24857a64d8
Kent Larsson <kent@c8.se>
2025-10-30 13:09:17 +0100

    autocommit complete 1309 (17)

--- ITINF24/lektion03/regexp/ex.py
+++ ITINF24/lektion03/regexp/ex.py
@@ -5,6 +5,6 @@ import re
 # psuedokod för regex som matchar ord på b
 # (först på raden|mellanslag)b(sist på raden|tecken som inte är a-z)
 # mer och mer till riktig kod.
-# (^|mellanslag)b(sist på raden|tecken som inte är a-z)
+# (^| )b(sist på raden|tecken som inte är a-z)
 
 # re.compile()
