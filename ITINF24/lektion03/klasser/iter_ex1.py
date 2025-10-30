92ac12f11ab0ec2af7b95f0d19fab57c1959e120
Kent Larsson <kent@c8.se>
2025-10-30 11:15:08 +0100

    autocommit complete 1115 (08)

--- ITINF24/lektion03/klasser/iter_ex1.py
+++ ITINF24/lektion03/klasser/iter_ex1.py
@@ -2,13 +2,6 @@
 
 import random
 
-# Animal       Attack       Health      Sound
-# donkey       20           80          "Ihhohhh"
-# giraffe      40           50          "..."
-# monkey       60           40          "Oh oh ah ah"
-# pinguin      10           10          "Meek! Meek!"
-# armadillo    5            100         "Piiiiip!"
-
 
 class Animal:
     def __init__(self, animal, attack, health, sound):
@@ -25,15 +18,23 @@ class Me:
         self.sound = sound
 
 
+# Animal       Attack       Health      Sound
+# donkey       20           80          "Ihhohhh"
+# giraffe      40           50          "..."
+# monkey       60           40          "Oh oh ah ah"
+# pinguin      10           10          "Meek! Meek!"
+# armadillo    5            100         "Piiiiip!"
+
+
 animals = [
-    {"animal": "donkey", "attack": 20, "health": 80, "sound": "Ihhohhh"},
-    {"animal": "giraffe", "attack": 40, "health": 50, "sound": "..."},
-    {"animal": "monkey", "attack": 60, "health": 40, "sound": "Oh oh ah ah"},
-    {"animal": "pinguin", "attack": 10, "health": 10, "sound": "Meek! Meek!"},
-    {"animal": "armadillo", "attack": 5, "health": 100, "sound": "Piiiiip!"},
+    Animal("donkey", 20, 80, "Ihhohhh"),
+    Animal("giraffe", 40, 50, "..."),
+    Animal("monkey", 60, 40, "Oh oh ah ah"),
+    Animal("pinguin", 10, 10, "Meek! Meek!"),
+    Animal("armadillo", 5, 100, "Piiiiip!"),
 ]
 
-me = {"attack": 15, "health": 40, "sound": "hej!"}
+me = Me(15, 40, "Hej!")
 
 animal = random.choice(animals)
 
