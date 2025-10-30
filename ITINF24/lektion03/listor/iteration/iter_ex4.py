e09d0be48335a3561f601b2fc7a6deb7e31c0d29
Kent Larsson <kent@c8.se>
2025-10-30 09:49:25 +0100

    autocommit complete 0949 (25)

--- ITINF24/lektion03/listor/iteration/iter_ex4.py
+++ ITINF24/lektion03/listor/iteration/iter_ex4.py
@@ -2,7 +2,6 @@
 
 animals = ["donkey", "giraffe", "monkey", "pinguin", "armadillo"]
 
-for animal in animals:
-    if "e" in animal:
-        continue
-    print("Me love", animal)
+good_animals = animals
+
+print(good_animals)
