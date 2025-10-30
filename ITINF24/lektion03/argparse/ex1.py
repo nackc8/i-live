05d294170d8968426e6c1030a7496b1b22ff8bde
Kent Larsson <kent@c8.se>
2025-10-30 13:47:16 +0100

    autocommit complete 1347 (16)

--- ITINF24/lektion03/argparse/ex1.py
+++ ITINF24/lektion03/argparse/ex1.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 
+# stringmod ([-u|--uppercase] | [-l|--lowercase]) FILE
+
+
+# - Definiera argument och flaggor med `ArgumentParser()`
+
+# - Ömsesidigt uteslutande grupper (`add_mutually_exclusive_group()`)
+
+# - Booleska flaggor och positionella argument
