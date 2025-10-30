bc76ea4460c07a735b129a7306c6def70aa395c8
Kent Larsson <kent@c8.se>
2025-10-30 09:57:12 +0100

    autocommit complete 0957 (12)

--- ITINF24/lektion03/listor/iteration/iter_ex5.py
+++ ITINF24/lektion03/listor/iteration/iter_ex5.py
@@ -2,4 +2,4 @@
 
 animals = ["donkey", "giraffe", "monkey", "pinguin", "armadillo"]
 
-good_animals = [animal for animal in animals if "e" in animal: continue]
+good_animals = [animal for animal in animals if "e" in animal]
