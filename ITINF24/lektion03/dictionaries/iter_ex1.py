8757bd3fc0454fba8ccc29d53ec1ae493c4b3ede
Kent Larsson <kent@c8.se>
2025-10-30 10:39:10 +0100

    autocommit complete 1039 (10)

--- ITINF24/lektion03/dictionaries/iter_ex1.py
+++ ITINF24/lektion03/dictionaries/iter_ex1.py
@@ -33,7 +33,7 @@ while me["health"] > 0 and animal["health"] > 0:
         success = True if random.randint(1, 100) > 50 else False
         if success:
             print(me["sound"])
-            animal["health"] -= me["attack"]
+            animal = animal["health"] - me["attack"]
             print(
                 f"The {animal['animal']} is hit for {me['attack']}, it has {animal['health']} health left."
             )
