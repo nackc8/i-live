d5a8b5e4b02e5532a87aea5ce7ab77d00bcff460
Kent Larsson <kent@c8.se>
2025-10-30 09:48:01 +0100

    autocommit partial 0948 (01)

---
+++ ITINF24/lektion03/listor/iteration/iter_ex3 copy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+animals = ["donkey", "giraffe", "monkey", "pinguin", "armadillo"]
+
+for animal in animals:
+    if "e" in animal:
+        continue
+    print("Me love", animal)
