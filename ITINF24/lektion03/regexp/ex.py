b41f2e52f0c02f86b5944883a302c17f5269f83c
Kent Larsson <kent@c8.se>
2025-10-30 13:25:52 +0100

    autocommit complete 1325 (52)

--- ITINF24/lektion03/regexp/ex.py
+++ ITINF24/lektion03/regexp/ex.py
@@ -9,17 +9,17 @@ import re
 # (^|tecken som inte är a-z)b(sist på raden|tecken som inte är a-z)
 #  \__ utbytt mot riktig kod
 
-# (^|[^a-z]+)b(sist på raden|[^a-z]+)
+# (^|[^a-z]+)b(sist på raden|[a-z]+)
 #      \__ utbytt mot riktig kod \__ utbytt mot riktig kod
 
 # (^|[^a-z]+)b($|[^a-z]+)
 #              \__ utbytt mot riktig kod
 
-# (^|[^a-zA-Z]+)[bB]($|[^a-zA-Z]+)
+# (^|[^a-zA-Z]+)[bB]($|[a-zA-Z]+)
 #                 \__ utbytt mot riktig kod
 
 # r-sträng, behåller alla tecken som de är skrivna
-ord_re = re.compile(r"(^|[^a-zA-Z]+)[bB]($|[^a-zA-Z]+)")
+ord_re = re.compile(r"(^|[^a-zA-Z]+)[bB]($|[a-zA-Z]+)")
 
 with open("test.txt") as file:
     content = file.read()
