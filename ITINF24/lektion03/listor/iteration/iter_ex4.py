fbe2b5bea0e843ff658ccaed87962e006be29123
Kent Larsson <kent@c8.se>
2025-10-30 09:54:17 +0100

    autocommit complete 0954 (17)

--- ITINF24/lektion03/listor/iteration/iter_ex4.py
+++ ITINF24/lektion03/listor/iteration/iter_ex4.py
@@ -2,10 +2,12 @@
 
 animals = ["donkey", "giraffe", "monkey", "pinguin", "armadillo"]
 
-good_animals = []
-for animal in animals:
-    good_animals.append(animal * 2)
+good_animals = [animal * 2 for animal in animals]
 
-# good_animals = [animal * 2 for animal in animals]
+# Exakt samma som:
+#
+# good_animals = []
+# for animal in animals:
+#     good_animals.append(animal * 2)
 
 print(good_animals)
