299a408a6a09c700fffe2255c17bae2a4107d128
Kent Larsson <kent@c8.se>
2025-10-30 11:43:40 +0100

    autocommit complete 1143 (40)

--- ITINF24/lektion03/klasser/iter_ex1.py
+++ ITINF24/lektion03/klasser/iter_ex1.py
@@ -2,22 +2,6 @@
 
 import random
 
-
-class Animal:
-    def __init__(self, animal, attack, health, sound):
-        self.animal = animal
-        self.attack = attack
-        self.health = health
-        self.sound = sound
-
-
-class Me:
-    def __init__(self, attack, health, sound):
-        self.attack = attack
-        self.health = health
-        self.sound = sound
-
-
 # Animal       Attack       Health      Sound
 # donkey       20           80          "Ihhohhh"
 # giraffe      40           50          "..."
@@ -25,65 +9,76 @@ class Me:
 # pinguin      10           10          "Meek! Meek!"
 # armadillo    5            100         "Piiiiip!"
 
-
 animals = [
-    Animal("donkey", 20, 80, "Ihhohhh"),
-    Animal("giraffe", 40, 50, "..."),
-    Animal("monkey", 60, 40, "Oh oh ah ah"),
-    Animal("pinguin", 10, 10, "Meek! Meek!"),
-    Animal("armadillo", 5, 100, "Piiiiip!"),
+    {"animal": "donkey", "attack": 20, "health": 80, "sound": "Ihhohhh"},
+    {"animal": "giraffe", "attack": 40, "health": 50, "sound": "..."},
+    {"animal": "monkey", "attack": 60, "health": 40, "sound": "Oh oh ah ah"},
+    {"animal": "pinguin", "attack": 10, "health": 10, "sound": "Meek! Meek!"},
+    {"animal": "armadillo", "attack": 5, "health": 100, "sound": "Piiiiip!"},
 ]
 
-me = Me(15, 40, "Hej!")
+name = ""
+while name == "":
+    name = input("What is your name? ")
+
+me = {"name": name, "attack": 15, "health": 40, "sound": "hej!"}
 
 animal = random.choice(animals)
 
-print(f"You encounter a wild {animal.animal}")
-while me.health > 0 and animal.health > 0:
-    print(f"Your health is {me.health}, the animals health is {animal.health}")
-    print(f"Your attack is {me.attack}, the animals attack is {animal.attack}")
+print(f"You encounter a wild {animal['animal']}")
+print(f"{me['name']}'s health is {me['health']} with attack {me['attack']}")
+print(
+    f"The {animal['animal']}'s health is {animal['health']}, with attack {animal['attack']}"
+)
 
-    action = ""
-    while not (action == "a" or action == "f"):
-        action = input("Attack or flee (a/f)? ")
+tried_to_flee = False
+while me["health"] > 0 and animal["health"] > 0:
+    if not tried_to_flee:
+        action = ""
+        while not (action == "a" or action == "f"):
+            action = input("Attack or flee (a/f)? ")
 
-    if action == "a":
-        success = True if random.randint(1, 100) > 50 else False
-        if success:
-            print(me["sound"])
-            next_animal_health = animal.health
-            next_animal_health -= me["attack"]
-            animal.health = next_animal_health
-            if animal.health > 0:
-                print(
-                    f"The {animal.animal} is hihhht for {me.attack}, it has {animal.health} health left.")
-       animal.attack       else:
-                print(f"The {animal.animal} is hit for {me.attack}, it perished.")
-            continue
-        else:
-            print("You missed!")
+        if action.lower() == "a":
+            success = True if random.randint(1, 100) > 50 else False
+            if success:
+                print(me["name"] + ": " + me["sound"])
+                next_animal_health = animal["health"]
+                next_animal_health -= me["attack"]
+                animal["health"] = next_animal_health
+                if animal["health"] > 0:
+                    print(
+                        f"The {animal['animal']} is hit for {me['attack']}, it has {animal['health']} health left."
+                    )
+                else:
+                    print(
+                        f"The {animal['animal']} is hit for {me['attack']}, it perished."
+                    )
+                    continue
+            else:
+                print(me["name"] + " missed!")
 
     animal_success = True if random.randint(1, 100) > 50 else False
     if animal_success:
-        print(animal["sound"])
+        print(animal["animal"] + ": " + animal["sound"])
         next_me_health = me["health"]
         next_me_health -= animal["attack"]
         me["health"] = next_me_health
         if me["health"] > 0:
             print(
-                f"You're hit for {animal['attack']}, you have {me.health} health left."
+                f"You're hit for {animal['attack']}, you have {me['health']} health left."
             )
         else:
-            print(f"The {animal.animal} is hit for {me.attack}, you are dead.")
+            print(f"The {animal['animal']} is hit for {me['attack']}, you are dead.")
             continue
-
     else:
-        print(f"The {animal.animal} missed!")
+        print(f"The {animal['animal']} missed!")
 
-    if action == "f":
+    if not tried_to_flee and action.lower() == "f":
+        tried_to_flee = True
         success_flee = True if random.randint(1, 100) > 50 else False
         if success_flee:
-            print("You cowardly ran away!")
+            print(f"{me['name']} cowardly ran away!")
             break
+    tried_to_flee = False
 
 print("Game over")
