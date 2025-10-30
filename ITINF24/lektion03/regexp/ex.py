790026d9f0016da97344033f611118f1f664d5b9
Kent Larsson <kent@c8.se>
2025-10-30 13:07:53 +0100

    autocommit complete 1307 (53)

--- ITINF24/lektion03/regexp/ex.py
+++ ITINF24/lektion03/regexp/ex.py
@@ -3,6 +3,6 @@
 import re
 
 # psuedokod för regex som matchar ord på b
-# (först på raden|mellanslag)b(sist på raden|)
+# (först på raden|mellanslag)b(sist på raden|tecken som inte är a-z)
 
 # re.compile()
