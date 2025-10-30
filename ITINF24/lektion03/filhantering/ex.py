73d023c941c04cf8e76712c9bf8ee8cb49413954
Kent Larsson <kent@c8.se>
2025-10-30 12:49:23 +0100

    autocommit complete 1249 (23)

--- ITINF24/lektion03/filhantering/ex.py
+++ ITINF24/lektion03/filhantering/ex.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
 with open("test.txt") as file:
-    print(file.read())
-    print(f"Oj! Den var {len(file.read())} tecken lång")
+    content = file.read()
+    print(content)
+    print(f"Oj! Den var {len(content)} tecken lång")
