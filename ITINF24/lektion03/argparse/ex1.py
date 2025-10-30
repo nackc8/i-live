aba0f459709854289f3777ee85f21a94c960da0a
Kent Larsson <kent@c8.se>
2025-10-30 14:01:32 +0100

    autocommit complete 1401 (32)

--- ITINF24/lektion03/argparse/ex1.py
+++ ITINF24/lektion03/argparse/ex1.py
@@ -15,6 +15,9 @@ parser.add_argument("filename", help="The filename to read and transform")
 parser.add_argument(
     "-l", action="store_true", help="Transform the file contents to lowercase"
 )
+parser.add_argument(
+    "-u", action="store_true", help="Transform the file contents to uppercase"
+)
 
 if __name__ == "__main__":
     parser.parse_args()
